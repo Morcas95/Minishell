0b36f3e59e0642d177e0acaa87cc7a53c7dcb3bb
Morcas95 <morcas@DESKTOP-DGNAOIQ.localdomain>
2026-02-03 20:23:04 +0100

    Estructuras

---
+++ src/lexer/lexer.c
@@ -0,0 +1,12 @@
+#include "minishell.h"
+
+/*
+ * Convierte un string en una lista de tokens
+ * Retorna: la lista de tokens, o NULL si hay error
+ */
+
+ t_token *lexer(char *input)
+{
+    (void)input;
+    return (NULL);
+}
