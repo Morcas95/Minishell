6c9bd9f87e70df04de9ee6e2ecf7d49eedba4586
Marcos Alonso Berenguer <maalonso@c2r9s4.42madrid.com>
2026-03-16 18:35:46 +0100

    FIX:
    Terminar de acortar el lexer

--- src/lexer/lexer_words.c
+++ src/lexer/lexer_words.c
@@ -1,31 +1,5 @@
 #include "minishell.h"
 
-static int	append_text(char **result, const char *text)
-{
-	char	*joined;
-
-	if (!text)
-		return (0);
-	joined = ft_strjoin(*result, (char *)text);
-	if (!joined)
-		return (-1);
-	*result = joined;
-	return (0);
-}
-
-static int	append_exit_status(char **result, int last_exit_status)
-{
-	char	*status;
-	int		r;
-
-	status = ft_itoa(last_exit_status);
-	if (!status)
-		return (-1);
-	r = append_text(result, status);
-	free(status);
-	return (r);
-}
-
 static int	append_env_var(const char *s, int *i, char **result, char **envp,
 		int last_exit_status)
 {
